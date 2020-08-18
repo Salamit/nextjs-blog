42647baf02b9066416b806b45a89da0a3f9bf3b7
T. Salami <salami.tope@gmail.com>
2020-08-18 21:56:59 +0900

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+// Use this page to import the global styles ie styles that affect the
+// whole app. 
+import '../styles/global.css'
+
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+
+}
